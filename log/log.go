532f76c07b27d0d74b629b81f1a7db178c5ec9da
qiuzhanghua <qiuzhanghua@icloud.com>
2023-11-06 13:11:18 +0800

    set Global Logging Level

--- log/log.go
+++ log/log.go
@@ -10,18 +10,23 @@ func SetGlobalLevel(level string) {
 	switch level {
 	case "trace":
 		zerolog.SetGlobalLevel(zerolog.TraceLevel)
+		break
 	case "debug":
 		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+		break
 	case "warn":
 		zerolog.SetGlobalLevel(zerolog.WarnLevel)
+		break
 	case "error":
 		zerolog.SetGlobalLevel(zerolog.ErrorLevel)
+		break
 	case "fatal":
 		zerolog.SetGlobalLevel(zerolog.FatalLevel)
+		break
 	case "panic":
 		zerolog.SetGlobalLevel(zerolog.PanicLevel)
+		break
 	default:
 		zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	}
-	zerolog.SetGlobalLevel(zerolog.DebugLevel)
 }
