ca07d3b9de24c8d8bc259c674fbd8fa9f2908bc5
qiuzhanghua <qiuzhanghua@icloud.com>
2023-11-09 15:38:41 +0800

    move replace-string from tiny-replace

---
+++ util/repl.go
@@ -0,0 +1,52 @@
+package util
+
+import (
+	"os"
+	"regexp"
+	"runtime"
+	"strings"
+)
+
+// ReplaceString replaces ${VAR} or %VAR% with the value of the environment variable VAR.
+func ReplaceString(s string, rep ...string) string {
+	var regex = regexp.MustCompile("[$][{].+?[}]|[%].+?[%]")
+	m := make(map[string]string, len(rep))
+	for _, r := range rep {
+		arr := strings.Split(r, "=")
+		if len(arr) == 2 {
+			m[strings.TrimSpace(arr[0])] = strings.TrimSpace(arr[1])
+		}
+	}
+	envs := regex.FindAllString(s, -1)
+	if len(envs) == 0 && len(m) == 0 {
+		return s
+	}
+	for _, e := range envs {
+		if len(e) < 3 {
+			continue
+		}
+		var e2 string
+		if strings.HasPrefix(e, "$") {
+			e2 = e[2 : len(e)-1]
+		} else {
+			e2 = e[1 : len(e)-1]
+		}
+		if len(m) > 0 {
+			val, ok := m[e2]
+			if ok {
+				s = strings.ReplaceAll(s, e, val)
+			}
+		} else {
+			env, ok := os.LookupEnv(e2)
+			if !ok {
+				continue
+			}
+			if runtime.GOOS == "windows" {
+				env = strings.ReplaceAll(env, "\\", "\\\\")
+
+			}
+			s = strings.ReplaceAll(s, e, env)
+		}
+	}
+	return s
+}
---
+++ util/repl_test.go
@@ -0,0 +1,39 @@
+package util
+
+import (
+	"os"
+	"testing"
+)
+
+func TestReplaceString(t *testing.T) {
+	content := "Hello, ${JAVA_HOME}"
+	_ = os.Setenv("JAVA_HOME", "/usr/bin")
+	actual := ReplaceString(content)
+	expected := "Hello, /usr/bin"
+	if actual != expected {
+		t.Errorf("Expected : %v, actual is '%v'", expected, actual)
+	}
+}
+
+func TestReplaceString2(t *testing.T) {
+	content := "Hello, %JAVA_HOME%"
+	_ = os.Setenv("JAVA_HOME", "/usr/bin")
+	actual := ReplaceString(content)
+	expected := "Hello, /usr/bin"
+	if actual != expected {
+		t.Errorf("Expected : %v, actual is '%v'", expected, actual)
+	}
+}
+
+func TestReplaceString3(t *testing.T) {
+	content := "setx /m JAVA_HOME ${TDP_HOME}/${TDP_LIB}/JAVA/${TDP_CURRENT}"
+	_ = os.Setenv("JAVA_HOME", "/usr/bin")
+	_ = os.Setenv("TDP_HOME", "tdp")
+	_ = os.Setenv("TDP_LIB", "lib")
+	_ = os.Setenv("TDP_CURRENT", "current")
+	actual := ReplaceString(content)
+	expected := "setx /m JAVA_HOME tdp/lib/JAVA/current"
+	if actual != expected {
+		t.Errorf("Expected : %v, actual is '%v'", expected, actual)
+	}
+}
